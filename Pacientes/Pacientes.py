7eec43106fb282baf1c3998bc11ff34039f39527
Coni-code-bit <lopezfumasconstanza@hotmail.com>
2025-04-28 15:44:19 -0300

    Create Pacientes.py
    pa

---
+++ Pacientes/Pacientes.py
@@ -0,0 +1,10 @@
+class Pacientes(): 
+    def __init__(self, nombre, DNI, fecha_nac, sexo, telefono, tipo_de_sangre): 
+        self.__nombre=nombre
+        self.__DNI=DNI
+        self.__fecha_nac=fecha_nac
+        self.__sexo=sexo
+        self.__telefono=telefono 
+        self.__tipo_de_sangre=tipo_de_sangre
+        
+    
